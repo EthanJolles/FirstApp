932aade7d95c2eeca22c1e78c637dd3b93ce7482
Ethan Jolles <erjolles@gmail.com>
2022-08-19 19:10:01 -0400

    package refactor

--- src/main/java/com/jolles/spring/security/firstapp/auth/config/MySecurityConfig.java
+++ src/main/java/com/jolles/spring/security/firstapp/auth/config/MySecurityConfig.java
@@ -1,48 +1,39 @@
-package com.jolles.spring.security.firstapp.config;
+package com.jolles.spring.security.firstapp.auth.config;
 
+import com.jolles.spring.security.firstapp.auth.MyAuthenticationProvider;
+import com.jolles.spring.security.firstapp.auth.MySecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private MyAuthenticationProvider authenticationProvider;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
-
-        UserDetails user = User
-                //Change to dynamic username later
-                .withUsername("Tom")
-                //Password encoder is necessary
-                //application is expecting an encoded password
-                //application will know if password does not look like BCrypt
-                .password(passwordEncoder.encode("Cruise"))
-                .authorities("read")
-                .build();
-        userDetailsService.createUser(user);
-
-        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //Sets up basic auth user/pass
         http.httpBasic();
+
+        //Sets up form based auth
+//        http.formLogin();
         //All requests need authentication
         //permitAll lets everyone in
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/hello").authenticated();
+
+        http.addFilterBefore(new MySecurityFilter(), BasicAuthenticationFilter.class);
     }
 
     @Bean
