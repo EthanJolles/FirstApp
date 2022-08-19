8395d62307ba5e107c3e835fa6f05cddab942d95
Ethan Jolles <erjolles@gmail.com>
2022-08-18 21:42:44 -0400

    Create HelloController.java

---
+++ src/main/java/com/jolles/spring/security/firstapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jolles.spring.security.firstapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Security Rocks!!";
+    }
+}
