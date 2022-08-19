0746a118f0d1110742eb8294664075faeb6be60f
Ethan Jolles <erjolles@gmail.com>
2022-08-18 21:42:51 -0400

    Create FirstAppApplicationTests.java

---
+++ src/test/java/com/jolles/spring/security/firstapp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolles.spring.security.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
